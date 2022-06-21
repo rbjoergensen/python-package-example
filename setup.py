24cfef63aeb4edfb49a9bd8d43b19180f44249ff
RBJ Rasmus Bo Jørgensen <rbj@sampension.dk>
2022-06-21 13:31:17 +0200

    +semver: major | Renamed package

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("Readme.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="callofthevoidexample",
+    name="callofthevoid-example",
     version=os.environ["BUILD_BUILDNUMBER"],
     author="Rasmus Jørgensen",
     author_email="rasmus@callofthevoid.dk",
     description="An example of how to create and publish a Python package",
-    packages=['test_package'],
+    packages=['test'],
     install_requires=[],
     long_description=long_description,
     long_description_content_type="text/markdown",
